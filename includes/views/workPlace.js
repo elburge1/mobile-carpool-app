a5eab72d003225a671330bc0c0eac64cae2593e2
Edward Louis Burgess <elburgess1@gmail.com>
2017-03-31 15:02:23 -0700

    start next view

---
+++ includes/views/workPlace.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import {
+  View,
+  Text,
+  TouchableHighlight,
+  StyleSheet,
+  TextInput,
+  Picker,
+} from 'react-native';
+
+const Item = Picker.Item;
+
+export default WorkPlace;
