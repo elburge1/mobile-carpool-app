5079ff215c0074e03c7cff8208a68a87ab58fc5d
Edward Louis Burgess <elburgess1@gmail.com>
2017-04-04 14:28:28 -0700

    more name changes

--- index.ios.js
+++ index.ios.js
@@ -3,8 +3,8 @@ import {
   AppRegistry,
 } from 'react-native';
 
-import SetPrefs from './includes/views/setPreference';
-import Profile from './includes/views/workPlace';
+import SetPrefs from './includes/views/SetPreference';
+import Profile from './includes/views/Profile';
 
 class Initial extends Component {
   constructor(props){
