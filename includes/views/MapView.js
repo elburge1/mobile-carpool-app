4261aeabb24e56a30494391c767377e9aab20f1f
Edward Louis Burgess <elburgess1@gmail.com>
2017-05-01 20:00:36 -0700

    add basic boilerplate

---
+++ includes/views/MapView.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import {
+  View,
+  Text,
+  TouchableHighlight,
+  StyleSheet,
+} from 'react-native';
+
+const styles = StyleSheet.create({
+
+})
+
+class MapView extends Component {
+  
+}
