c79c60a0e9cd149b6d98f8b80962a30b6f57620e
Edward Louis Burgess <elburgess1@gmail.com>
2017-05-03 13:49:47 -0700

    add boilerplate code to map page

--- includes/views/RouteMap.js
+++ includes/views/RouteMap.js
@@ -1,17 +1,30 @@
 import React, { Component } from 'react';
+import MapView from 'react-native-maps';
 import {
   View,
   Text,
   TouchableHighlight,
   StyleSheet,
+  Dimensions,
 } from 'react-native';
+
+var { width, height } = Dimensions.get('window');
+
+const ASPECT_RATIO = width / height;
+const LATITUDE = 47.6253;
+const LONGITUDE = -122.3222;
+const LATITUDE_DELTA = 0.0922;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+const SPACE = 0.01;
 
 const styles = StyleSheet.create({
 
 })
 
 class RouteMap extends Component {
-
+  render(){
+    
+  }
 }
 
 export default RouteMap;
